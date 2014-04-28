cab2279ae513a2c5b62542309e751e48c023cce9
JoseJRVazquez <kwikteck@gmail.com>
2014-04-28 18:47:13 -0500

    Associated and nested topics and posts: It has been a bitch to finish, there I said it, so deal with!

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 #This calls the Resources method, passes the symbol to it, and instructs rails to create routes for each CRUD action
 Bloccit::Application.routes.draw do
 
+
   devise_for :users
-  resources :posts
+
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   get 'about' => 'welcome#about'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require 'faker'
-
+ 
 # Create Users
 5.times do
   user = User.new(
@@ -11,26 +11,29 @@ require 'faker'
   user.save
 end
 users = User.all
-
-# Note: by calling `User.new` instead of `create`,
-# we create an instance of User which isn't immediately saved to the database.
-
-# The `skip_confirmation!` method sets the `confirmed_at` attribute
-# to avoid triggering an confirmation email when the User is saved.
-
-# The `save` method then saves this User to the database.
-
-
+ 
+ 
+# Create Topics
+15.times do
+  Topic.create(
+    name:         Faker::Lorem.sentence,
+    description:  Faker::Lorem.paragraph 
+  )         
+end
+topics = Topic.all
+ 
+ 
 # Create Posts
 50.times do
   Post.create(
     user:   users.sample,
+    topic: topics.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
   )
 end
 posts = Post.all
-
+ 
 # Create Comments
 100.times do
   Comment.create(
@@ -39,12 +42,47 @@ posts = Post.all
     body: Faker::Lorem.paragraph
   )
 end
-
-User.first.update_attributes(
-  email: 'jr.vazquez.11@gmail.com',
-  password: 'whyaskwhy',
+ 
+# Create an admin user
+admin = User.new(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld'
 )
-
+admin.update_attribute(:role, 'admin')
+admin.skip_confirmation!
+if admin.save
+  puts "Admin User Create"
+else 
+  admin.errors.full_messages.each do |message|
+    puts message
+  end
+end
+# Create an Moderator user
+moderator = User.new(
+  name:     'Moderator User',
+  email:    'moderator@example.com',
+  password: 'helloworld'
+)
+moderator.update_attribute(:role, 'moderator')
+moderator.skip_confirmation!
+if moderator.save
+  puts "Moderator User Create"
+else 
+  moderator.errors.full_messages.each do |message|
+    puts message
+  end
+end
+ 
+# Create an Member user
+member = User.new(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
+)
+member.skip_confirmation!
+member.save
+ 
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Post.count} posts created"
