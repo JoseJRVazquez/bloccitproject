b84ddd3f6b315c66c211c5c8405e59beade74afc
JoseJRVazquez <kwikteck@gmail.com>
2014-05-03 17:37:54 -0500

    Vote count addition and changes
    Added votes view along with adding the vote counts to the post model.
    In addition we created the voter partial file for later use. App
    functional currently.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   belongs_to :user
   belongs_to :topic
-  mount_uploader :image
+  mount_uploader :image  #, ImageUploader #add this after correcting
   has_many :votes, dependent: :destroy
 
   default_scope { order('created_at DESC') }
@@ -11,4 +11,16 @@ class Post < ActiveRecord::Base
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+
+  def up_votes #counts up votess
+    self.votes.where(value: 1).count
+  end
+
+  def down_votes #counts down votes
+    self.votes.where(value: -1).count
+  end
+
+  def points #shows the points accumulated
+    self.votes.sum(:value).to_i
+  end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,8 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+
+  #validates the vote, and will reject non-matching votes, but god knows how that happens
+  validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vote." }
 end
+
