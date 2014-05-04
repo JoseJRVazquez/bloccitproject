8f8500e185180c80b5b5d97259cf4b6cd0b9d6c3
JoseJRVazquez <kwikteck@gmail.com>
2014-05-04 16:44:13 -0500

    Ajaxified comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -46,6 +46,8 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(
+      :body
+      :post_id)
   end
 end
