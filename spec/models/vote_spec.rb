659bf4dec465c1a0f31c50389f33dc2f8ed20cff
JoseJRVazquez <kwikteck@gmail.com>
2014-05-04 14:30:35 -0500

    Implemented testing suite and created tests for Vote

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,12 +1,10 @@
 require 'spec_helper'
 
-  describe "#update_post" do
-    it "calls `update_rank` on post" do
-      Post.skip_callback(:create,:after,:create_vote)
-      post = Post.new
-      post.save(validate: false)
-      post.should_receive(:update_rank)
-      Vote.create(value: 1, post: post)
-      Post.set_callback(:create, :after, :create_vote)
-    end
+describe "#update_post" do
+  it "calls `update_rank` on post" do
+    post = create(:post)
+    post.should respond_to(:update_rank)
+    post.should_receive(:update_rank)
+    Vote.create(value: 1, post: post)
   end
+end
